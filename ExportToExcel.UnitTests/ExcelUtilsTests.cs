1659f562eac184639ed07bb9db9017ff3c5fcba5
kajan <gmkajan@gmail.com>
2021-03-08 00:12:01 +0530

    assert `GetExcelColumnName` behavior

---
+++ ExportToExcel.UnitTests/ExcelUtilsTests.cs
@@ -0,0 +1,38 @@
+﻿// ExportToExcel/UnitTests/ExcelUtilsTests.cs
+
+using System.Collections.Generic;
+using ExportToExcel.Services.Utils;
+using Xunit;
+
+namespace ExportToExcel.UnitTests
+{
+    public class ExcelUtilsTests
+    {
+        [Fact]
+        public void GetExcelColumnName_Should_Behave_As_Expected()
+        {
+            #region Arrange
+
+            var inputVsExpectedOutput = new Dictionary<int, string>
+            {
+                {1, "A"},
+                {26, "Z"},
+                {28, "AB"},
+                {53, "BA"}
+            };
+
+            #endregion
+
+            #region ActAndAssert
+
+            foreach (var (input, expectedOutput) in inputVsExpectedOutput)
+            {
+                var output = ExcelUtils.GetExcelColumnName(input);
+
+                Assert.Equal(output, expectedOutput);
+            }
+
+            #endregion
+        }
+    }
+}
