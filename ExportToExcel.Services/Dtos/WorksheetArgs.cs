93fc3779ae00b1aa32b1a1bc19f5444b33e7bad6
kajan <gmkajan@gmail.com>
2021-03-07 15:39:40 +0530

    define generate excel contract

---
+++ ExportToExcel.Services/Dtos/WorksheetArgs.cs
@@ -0,0 +1,16 @@
+﻿// ExportToExcel/Services/Dtos/WorksheetArgs.cs
+
+using System.Collections.Generic;
+
+namespace ExportToExcel.Services.Dtos
+{
+    /// <summary>
+    /// Contains arguments required to generate an Excel worksheet
+    /// </summary>
+    public class WorksheetArgs<ViewModelType>
+    {
+        public string Title { get; set; }
+
+        public IEnumerable<ViewModelType> Records { get; set; }
+    }
+}
---
+++ ExportToExcel.Services/IGenerateExcelService.cs
@@ -0,0 +1,18 @@
+﻿// ExportToExcel/Services/IGenerateExcelService.cs
+
+using System.Threading.Tasks;
+using ExportToExcel.Services.Dtos;
+
+namespace ExportToExcel.Services
+{
+    public interface IGenerateExcelService
+    {
+        IGenerateExcelService SetWorkbookTitle(string title);
+    
+        IGenerateExcelService AddWorksheet<ViewModelType>(WorksheetArgs<ViewModelType> worksheetArgs);
+    
+        IGenerateExcelService SetDateFormat(string dateFormat);
+    
+        Task<byte[]> GenerateExcelAsync();
+    }
+}
